7e725fe51ef4466fb02b1a39da944d3437f69034
Jorge Juvenal Campos Ferreira <41249739+JuveCampos@users.noreply.github.com>
2019-02-08 12:55:07 -0600

    Add files via upload

--- R - leaflet/Mapas_zona_metropolitana/Mapa_zm.R
+++ R - leaflet/Mapas_zona_metropolitana/Mapa_zm.R
@@ -1,21 +1,9 @@
-mapa_zm <- function(colorHGO = "Yellow", colorCDMX = "Red", colorEDOMEX = "Green") {
-  # Bases de datos
-  # MUNICIPIOS ZMVM
+mapa_zm <- function(colorHGO = "Yellow", colorCDMX = "Red", colorEDOMEX = "Green", labelMapa = paste0(ZMCM$NOM_MUN, ", ", ZMCM$ESTADO.C.31)) {
   ZMCM <- st_read('https://github.com/JuveCampos/Shapes_Resiliencia_CDMX_CIDE/raw/master/Zona%20Metropolitana/EdosZM.geojson') 
-  # ESTADOS ZMVM
   ZMCM_edos <- st_read('https://github.com/JuveCampos/Shapes_Resiliencia_CDMX_CIDE/raw/master/Zona%20Metropolitana/EstadosZMVM.geojson')
-  
-  
-  # Paleta de Colores
-  pal = colorFactor(palette = c("Red", "Yellow", "Green"), domain = c("09", "13", "15"))
-  
-  # Leaflet
+  pal = colorFactor(palette = c(colorCDMX, colorHGO, colorEDOMEX), domain = c("09", "13", "15"))
   leaflet() %>%
-    addPolygons(data = ZMCM, label = paste0(ZMCM$NOM_MUN, ", ", ZMCM$ESTADO.C.31), weight = 0.9, dashArray ="3,3",
+    addPolygons(data = ZMCM, label = labelMapa, weight = 0.9, dashArray ="3,3",
                 fillColor = ~pal(ZMCM$CVE_ENT), color = "gray") %>%
     addPolygons(data = ZMCM_edos, color="black", weight = 2, fill = F)
 }
-
-mapa_zm <- mapa_zm()
-saveRDS(mapa_zm, "Mapa_zm.rds")
-
